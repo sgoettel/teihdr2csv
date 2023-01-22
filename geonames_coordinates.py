7b0f2302a694ada7b7ffd6605dd9c93bc48c653b
Sebastian Göttel <33727473+sgoettel@users.noreply.github.com>
2023-01-22 11:45:42 +0100

    Add files via upload

---
+++ geonames_coordinates.py
@@ -0,0 +1,78 @@
+import csv
+import requests
+import re
+
+def get_coordinates(place_id):
+    url = f'https://sws.geonames.org/{place_id}/about.rdf'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Parse the XML content of the response
+            xml_content = response.content
+            xml_content = xml_content.decode()
+            # Extract the latitude and longitude coordinates
+            lat_match = re.search(r'<wgs84_pos:lat>([^<]+)</wgs84_pos:lat>', xml_content)
+            long_match = re.search(r'<wgs84_pos:long>([^<]+)</wgs84_pos:long>', xml_content)
+            if lat_match and long_match:
+                lat = lat_match.group(1)
+                long = long_match.group(1)
+                return (lat, long)
+            else:
+                print(f'Could not find coordinates for place ID {place_id}')
+        else:
+            print(f'Error getting coordinates for place ID {place_id}')
+    except requests.exceptions.RequestException as e:
+        print(f'Error getting coordinates for place ID {place_id}: {e}')
+
+def main():
+    row_counter = 0
+    with open('teihdr_output.csv', 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader) # save the header
+        data = []
+        for i,row in enumerate(reader):
+            letter_id, file_name, sender_name, sender_id, sender_place, sender_place_id, sender_place_lat, sender_place_long, date_sent, receiver_name, receiver_id, receiver_place, receiver_place_id, receiver_place_lat, receiver_place_long, title, url = row
+
+
+            if sender_place_id and sender_place_id.startswith("https://www.geonames.org"):
+                place_id = sender_place_id.split('/')[-1]
+                lat, long = get_coordinates(place_id)
+                if lat is not None and long is not None:
+                    row[6] = lat
+                    row[7] = long
+                else:
+                    print(f"Couldn't find coordinates for sender place ID {sender_place_id} in row {i+1}")
+                    row[6] = ""
+                    row[7] = ""
+            elif sender_place_id:
+                print(f"Invalid sender place ID {sender_place_id} in row {i+1}")
+                row[6] = ""
+                row[7] = ""
+            else:
+                row[6] = ""
+                row[7] = ""
+                
+            if receiver_place_id and receiver_place_id.startswith("https://www.geonames.org"):
+                place_id = receiver_place_id.split('/')[-1]
+                lat, long = get_coordinates(place_id)
+                if lat is not None and long is not None:
+                    row[13] = lat
+                    row[14] = long
+                else:
+                    print(f"Couldn't find coordinates for receiver place ID {receiver_place_id} in row {i+1}")
+                    row[13] = ""
+                    row[14] = ""
+            elif receiver_place_id:
+                print(f"Invalid receiver place ID {receiver_place_id} in row {i+1}")
+                row[13] = ""
+                row[14] = ""
+            else:
+                row[13] = ""
+                row[14] = ""
+            data.append(row)
+    with open("teihdr_output.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+if __name__ == '__main__':
+    main()
